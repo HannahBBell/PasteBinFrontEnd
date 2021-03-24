5a97d1807e8620c68d89263dad054f1d07a640f9
Hannah Bernard-Bell <hannah.bernard12@gmail.com>
2021-03-24 12:11:08 +0000

    MappedPostCard function implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import PostCard from './components/PostCard';
+import MappedPostCard from './components/PostCard';
 
-interface postTypes {
+export interface postTypes {
   id: number,
   title: string | null,
   content: string
@@ -46,6 +46,7 @@ function App() {
   return (
     <div className="App">
       <h1>HMPasteBin</h1>
+      <p>Disclaimer: The website has been designed to look GROSS</p>
       <label className="label" htmlFor="title">Title</label>
       <input value={titleInput} id="title" type="text" placeholder="Enter post title..." onChange={event => setTitleInput(event.target.value)}></input>
       <br/>
@@ -58,7 +59,7 @@ function App() {
       <br/>
       <br/>
       <hr/>
-      <PostCard/>
+      <MappedPostCard allPosts={allPosts}/>
     </div>
   );
 }
--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,9 +1,10 @@
 import './component.css'
+import {postTypes} from '../App';
 
-const filler = "She wanted rainbow hair. That's what she told the hairdresser. It should be deep rainbow colors, too. She wasn't interested in pastel rainbow hair. She wanted it deep and vibrant so there was no doubt that she had done this on purpose.She wanted rainbow hair. That's what she told the hairdresser. It should be deep rainbow colors, too. She wasn't interested in pastel rainbow hair. She wanted it deep and vibrant so there was no doubt that she had done this on purpose."
+// const filler = "She wanted rainbow hair. That's what she told the hairdresser. It should be deep rainbow colors, too. She wasn't interested in pastel rainbow hair. She wanted it deep and vibrant so there was no doubt that she had done this on purpose.She wanted rainbow hair. That's what she told the hairdresser. It should be deep rainbow colors, too. She wasn't interested in pastel rainbow hair. She wanted it deep and vibrant so there was no doubt that she had done this on purpose."
 
-export default function PostCard() {
-
+function PostCard(props: postTypes) {
+    const{title, content} = props //destructure props
     function stringTrimmer(filler:string) {
         if (filler.length >= 200) {
             const trimmedString = filler.substring(0, 200);
@@ -15,8 +16,22 @@ export default function PostCard() {
 
     return (
     <div className="PostCard">
-        <h2>Title</h2>
-        <p>{stringTrimmer(filler)}</p>
+        <h2>{title}</h2>
+        <p>{stringTrimmer(content)}</p>
     </div>
     )};
+
+//interface so we can type 'allPosts' useState which is an array of objects
+interface IMappedPostCard{ 
+    allPosts: postTypes[], //postTypes is another interface which defines types of each 'post'
+}    
+
+//mapping over all elements in array allPosts
+export default function MappedPostCard(props: IMappedPostCard) {
+    const{allPosts} = props
+    return (
+        <div>
+        {allPosts.map(PostCard)} 
+        </div>
+    )}
 
